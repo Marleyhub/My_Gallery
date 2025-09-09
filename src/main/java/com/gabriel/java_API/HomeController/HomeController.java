293fbb3d80cf817ea8e806cadc0bb2da515e6b4b
cardoso <cardoso.gab@outlook.com>
2025-09-09 17:42:32 -0300

    base of front-end

---
+++ src/main/java/com/gabriel/java_API/HomeController/HomeController.java
@@ -0,0 +1,18 @@
+package com.gabriel.java_API.HomeController;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+
+public class HomeController {
+	
+	@PostMapping("/processar")
+	
+	public String processarFormulario(@RequestParam String name, @RequestParam int age, Model model) {
+		
+		String str = "isso";
+		
+		return str;
+	}
+}

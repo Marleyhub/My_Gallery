a7dba6e7282f194cec514ac677062b624d55bb41
cardoso <cardoso.gab@outlook.com>
2025-09-11 10:46:05 -0300

    typo

--- src/main/java/com/gabriel/java_API/Server.java
+++ src/main/java/com/gabriel/java_API/Server.java
@@ -1,7 +1,9 @@
 package com.gabriel.java_API;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 
 @SpringBootApplication

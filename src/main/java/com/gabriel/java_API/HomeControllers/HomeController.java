3f67a9beead1d83c0642a418d8338ac59dfc7c44
cardoso <cardoso.gab@outlook.com>
2025-09-11 10:47:59 -0300

    adding HomeController to manage routes

---
+++ src/main/java/com/gabriel/java_API/HomeControllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.gabriel.java_API.HomeControllers;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//import java.util.Map;
+
+
+
+@RestController
+public class HomeController {
+	
+    @PostMapping("/api/user")
+    public String processUser() {
+    	System.out.println("foi"); 
+        return ""; // echoes back
+	}
+}

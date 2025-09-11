643a926b881664f0eea2831f9f9fc95dd8ba71dd
cardoso <cardoso.gab@outlook.com>
2025-09-11 12:34:24 -0300

    ajusting call

---
+++ src/main/java/com/gabriel/java_API/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.gabriel.java_API.dto;
+
+public class UserDto {
+	
+	private String userId;
+	private String password;
+	
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+}

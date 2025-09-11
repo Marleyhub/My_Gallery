762d985fb1e20b35f082d19ab56d2e22f6301869
cardoso <cardoso.gab@outlook.com>
2025-09-11 11:47:07 -0300

    taking out thymeleaf from configurations

--- src/main/java/com/gabriel/java_API/HomeControllers/HomeController.java
+++ src/main/java/com/gabriel/java_API/HomeControllers/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class HomeController {
+public class HomeController  {
 	
     @PostMapping("/api/user")
     public String processUser() {

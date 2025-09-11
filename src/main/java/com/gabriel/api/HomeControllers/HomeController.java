be3df7366e463e28ddf50868006c2301d9350853
cardoso <cardoso.gab@outlook.com>
2025-09-11 13:48:36 -0300

    HomeController now sending proper JSON

--- src/main/java/com/gabriel/api/HomeControllers/HomeController.java
+++ src/main/java/com/gabriel/api/HomeControllers/HomeController.java
@@ -1,20 +1,35 @@
 package com.gabriel.api.HomeControllers;
 
+import java.util.HashMap;
+import java.util.Map;
 
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.gabriel.api.services.UserService;
 import com.gabriel.api.dto.UserDto;
-
-
 
 
 @RestController
 @RequestMapping("/api")
 public class HomeController  {
+
+    private final UserService userService;
+    
+	public HomeController(UserService userService) {
+		this.userService = userService;
+	}
 	
     @PostMapping("/user")
-    public UserDto  processUser(@RequestBody UserDto user) {
-    	System.out.println(user.getUserId() + " - " + user.getPassword());
-        return user; // echoes back
+    public Map<String, Object>  processUser(@RequestBody UserDto user) {
+    	boolean isValid = userService.validateUser(user.getUserId(), user.getPassword());
+    	
+    	Map<String, Object> response = new HashMap<>();
+        response.put("success", isValid);
+        response.put("message", isValid ? "Login successful" : "Invalid credentials");
+
+        return response;
+    	
 	}
 }

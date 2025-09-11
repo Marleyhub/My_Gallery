9d38063db71dd39fc8c5801b1db260daefb4bb61
cardoso <cardoso.gab@outlook.com>
2025-09-11 10:46:55 -0300

    new console.log

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,15 +1,17 @@
 var app = angular.module('myApp', []);
 
+
 app.controller('UserController', function($http) {
     var vm = this;
     vm.user = {};
 
     // Call Spring Boot API
 	vm.submitForm = function() {
-	        $http.post('/api/user', vm.user)
+		console.log("Submitting:", vm.user);
+	        $http.post("/api/user", vm.user)
 	            .then(function(response) {
 	                alert('User sent successfully!');
-	                console.log(response.data);
+	                 console.log("Success:", response.data);
 	            }, function(error) {
 	                alert('Error sending user: ' + error);
 	            });

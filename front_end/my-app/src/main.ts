7fbc3f65d038e512d10e87294ed447d0c31d0d7c
cardoso <cardoso.gab@outlook.com>
2025-09-17 19:10:32 -0300

    adding bootstrap call for gallery component

--- front_end/my-app/src/main.ts
+++ front_end/my-app/src/main.ts
@@ -5,6 +5,7 @@ import { importProvidersFrom } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app/app';
 import { ToolBar } from './app/tool-bar/tool-bar';
+import { GalleryGrid } from './app/gallery/gallery';
 import { routes } from './app/app.routes';  // define your routes in app.routes.ts
 
 bootstrapApplication(AppComponent, {
@@ -15,6 +16,13 @@ bootstrapApplication(AppComponent, {
 .catch(err => console.error(err));
 
 bootstrapApplication(ToolBar, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(routes))
+  ]
+})
+.catch(err => console.error(err));
+
+bootstrapApplication(GalleryGrid, {
   providers: [
     importProvidersFrom(RouterModule.forRoot(routes))
   ]

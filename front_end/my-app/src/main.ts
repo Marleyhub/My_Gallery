c700a594041d3d634c7f94056cba18dd1ab04ff5
cardoso <cardoso.gab@outlook.com>
2025-09-17 18:39:46 -0300

    adding proper imports and bootstraping for the new toolbar

--- front_end/my-app/src/main.ts
+++ front_end/my-app/src/main.ts
@@ -4,9 +4,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app/app';
+import { ToolBar } from './app/tool-bar/tool-bar';
 import { routes } from './app/app.routes';  // define your routes in app.routes.ts
 
 bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(routes))
+  ]
+})
+.catch(err => console.error(err));
+
+bootstrapApplication(ToolBar, {
   providers: [
     importProvidersFrom(RouterModule.forRoot(routes))
   ]

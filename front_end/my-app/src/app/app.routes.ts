0fbb398b550d82d2242c175aec32bfac5c8a78ce
cardoso <cardoso.gab@outlook.com>
2025-09-18 13:36:02 -0300

    new app.route to serve files

--- front_end/my-app/src/app/app.routes.ts
+++ front_end/my-app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/index', pathMatch: "full"},
+    {
+        path: 'index',
+        loadComponent: () => import('./index/index-page/index').then(m => m.IndexPage)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./login/login-page/login').then(m =>m.LoginPage)
+    },
+    {
+        path: 'gallery',
+        loadComponent: () => import('./gallery/gallery-page/gallery').then(m =>m.GalleryPage)
+    }
+];

7f345d246b6c0aa185338d61c8c087aed56adff3
cardoso <cardoso.gab@outlook.com>
2025-09-20 15:54:02 -0300

    new userDto class and logic

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/dto/UserDto.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/dto/UserDto.java
@@ -2,4 +2,36 @@ package com.github.marleyhub.my_gallery.dto;
 
 public class UserDto {
 
+	private Long id;
+	private String email;
+	
+	public UserDto() {
+		
+	}
+	
+	public UserDto(Long id, String email) {
+		this.id = id;
+		this.email = email;
+	}
+	
+	public UserDto(com.github.marleyhub.my_gallery.entities.User user) {
+		this.id = user.getId();
+		this.email = user.getEmail();
+	}
+	
+	public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

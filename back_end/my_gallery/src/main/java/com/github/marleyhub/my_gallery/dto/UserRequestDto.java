bf03e02e7b80052f8ee4ed0241179561af80826b
cardoso <cardoso.gab@outlook.com>
2025-09-20 16:15:58 -0300

    new UserRequestDto class logic

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/dto/UserRequestDto.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/dto/UserRequestDto.java
@@ -2,4 +2,31 @@ package com.github.marleyhub.my_gallery.dto;
 
 public class UserRequestDto {
 
+    private String email;
+    private String password;
+
+    public UserRequestDto() {
+    }
+
+    public UserRequestDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters e Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }

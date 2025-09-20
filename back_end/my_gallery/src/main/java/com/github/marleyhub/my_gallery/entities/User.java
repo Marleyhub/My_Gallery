8d8a94567d55e31c55df221bf5f95ded55193bba
cardoso <cardoso.gab@outlook.com>
2025-09-20 15:29:40 -0300

    Adding user class

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/entities/User.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/entities/User.java
@@ -1,5 +1,71 @@
 package com.github.marleyhub.my_gallery.entities;
 
-public class User {
+import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition = "TEXT")
+	private String email;
+	private String password;
+	
+	public User(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail(){
+		return this.email;
+	}
+	
+	@Override
+	public String toString() {
+		return "User email: " + this.email + "User Id: " + this.id;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass()!= obj.getClass())
+			return false;
+		User other = (User) obj;
+		
+		return Objects.equals(id, other.id);
+	}
 }

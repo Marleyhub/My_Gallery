6bc9953310679f15ddc8b68edc1482195cdc1b77
cardoso <cardoso.gab@outlook.com>
2025-09-22 16:53:48 -0300

    Adding controllers package

---
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.github.marleyhub.my_gallery.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.marleyhub.my_gallery.entities.User;
+import com.github.marleyhub.my_gallery.services.UserService;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.printAllUsers();
+    }
+}

8abbad35dfbb2ec64b947a6f1d8b2b8a580e9817
cardoso <cardoso.gab@outlook.com>
2025-09-22 15:15:06 -0300

    import to SQL

---
+++ back_end/my_gallery/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_user (email, password) VALUES ('123@gmail.com', '123456789');
+INSERT INTO tb_user (email, password) VALUES ('456@gmail.com', '123456789');
+INSERT INTO tb_user (email, password) VALUES ('789@gmail.com', '123456789');
+INSERT INTO tb_user (email, password) VALUES ('1011@gmail.com', '123456789');
+INSERT INTO tb_user (email, password) VALUES ('1213@gmail.com', '123456789');

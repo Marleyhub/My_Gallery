d4be91e2a301cbf5da88f46e6a9ef463178340cb
cardoso <cardoso.gab@outlook.com>
2025-09-23 14:31:39 -0300

    updateUser new service

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/services/UserService.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/services/UserService.java
@@ -1,8 +1,10 @@
 package com.github.marleyhub.my_gallery.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.marleyhub.my_gallery.entities.User;
 import com.github.marleyhub.my_gallery.repositories.UserRepository;
@@ -15,16 +17,34 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 	
+	@Transactional(readOnly = true)
 	public List<User> printAllUsers() {
 		return userRepository.findAll();
 	}
   
+	@Transactional
     public List<User> createUser(User body) {
     	User result = userRepository.save(body);
     	return List.of(result);
     }
     
+    @Transactional
     public void deleteUser(Long id) {
     	userRepository.deleteById(id);
+    }
+    
+    @Transactional
+    public Optional<User> updateUser(Long id, User body) {
+    	if (body == null) {
+    		return Optional.empty();
+    	}
+    	
+    	return userRepository.findById(id).map(existingUser -> {
+    		existingUser.setEmail(body.getEmail());
+    		
+    		return userRepository.save(existingUser);
+    	});
+    	
+    	
     }
 }

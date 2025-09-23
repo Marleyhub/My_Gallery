5025a5a4a7a961a3ea6276532dd2e8a5c1537970
cardoso <cardoso.gab@outlook.com>
2025-09-23 13:29:59 -0300

    fixing route calls to match RESTfull and adding route createUser

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/controllers/UserController.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/controllers/UserController.java
@@ -3,12 +3,16 @@ package com.github.marleyhub.my_gallery.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.marleyhub.my_gallery.entities.User;
 import com.github.marleyhub.my_gallery.services.UserService;
 
 @RestController
+@RequestMapping(value = "/users")
 public class UserController {
     private final UserService userService;
 

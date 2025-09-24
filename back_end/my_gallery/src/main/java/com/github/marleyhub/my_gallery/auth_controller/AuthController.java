9401c0346474e14711cd29ddde61966eff971591
cardoso <cardoso.gab@outlook.com>
2025-09-24 20:01:02 -0300

    Validation of the email by userRepository

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/auth_controller/AuthController.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/auth_controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.github.marleyhub.my_gallery.auth_controller;
 
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,16 +9,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.marleyhub.my_gallery.entities.User;
+import com.github.marleyhub.my_gallery.services.UserService;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/auth")
 public class AuthController {
+	
+	private final UserService userService;
+	
+	public AuthController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	
 	
 	@PostMapping("/login")
 	public ResponseEntity <Map<String, String>> login(@RequestBody Map<String, String> credentials) {
 		String email = credentials.get("email");
 		String password = credentials.get("password");
 		
-		if("sabadaopelalapa@gmail.com".equals(email) && "1234567".equals(password)) {
+		Optional<User> result = userService.getOneUser(email);
+		String userEmail = result.map(u -> u.getEmail()).orElse("No user with this email");
+		
+		if(userEmail.equals(email) && "1234567".equals(password)) {
 			return ResponseEntity.ok(Map.of("massage", "login successful"));
 		} else {
 			return ResponseEntity.status(401).body(Map.of("message", "Invalid user or password"));

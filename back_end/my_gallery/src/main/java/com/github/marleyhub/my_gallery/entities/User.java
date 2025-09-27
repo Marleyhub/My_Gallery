05cb2d5a16556c204523b2663728e3e3c4195518
cardoso <cardoso.gab@outlook.com>
2025-09-27 19:48:21 -0300

    adding id to contructor

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/entities/User.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/entities/User.java
@@ -22,7 +22,8 @@ public class User {
 	private String email;
 	private String password;
 	
-	public User(String email, String password) {
+	public User(Long id, String email, String password) {
+		this.id = id;
 		this.email = email;
 		this.password = password;
 	}

b1c5bc22a64f92df6d4a6245fdcb77828b15e425
cardoso <cardoso.gab@outlook.com>
2025-09-27 19:49:02 -0300

    retunig DTO to controllers

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/services/UserService.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/services/UserService.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.github.marleyhub.my_gallery.dto.UserDto;
 import com.github.marleyhub.my_gallery.entities.User;
+import com.github.marleyhub.my_gallery.mapper.UserMapper;
 import com.github.marleyhub.my_gallery.repositories.UserRepository;
 
 @Service
@@ -15,37 +17,38 @@ public class UserService {
 	
 	public UserService(UserRepository userRepository) {
 		this.userRepository = userRepository;
+		
 	}
 	
 	// get all users
 	@Transactional(readOnly = true)
-	public List<User> getUsers() {
-		return userRepository.findAll();
+	public List<UserDto> getUsers() {
+		 return UserMapper.toDto(userRepository.findAll());
 	}
 	
 	// get one user by id
 	@Transactional(readOnly = true)
-	public Optional<User> getUser(Long id) {
+	public Optional<UserDto> getUser(Long id) {
 		if(id == null) {
 			return Optional.empty();
 		}
-		return userRepository.findById(id);
+		return UserMapper.toDto(userRepository.findById(id));
 	}
 	
 	// get one user by email
 	@Transactional(readOnly = true)
-	public Optional<User> getOneUser(String email) {
+	public Optional<UserDto> getOneUser(String email) {
 		if(email == null) {
 			return Optional.empty();
 		}
-		return userRepository.findByEmail(email);
+		return UserMapper.toDto(userRepository.findByEmail(email));
 	}
   
 	// create user
 	@Transactional
-    public List<User> createUser(User body) {
+    public UserDto createUser(User body) {
     	User result = userRepository.save(body);
-    	return List.of(result);
+    	return UserMapper.toDto(result);
     }
     
 	// delete user
@@ -56,15 +59,14 @@ public class UserService {
     
     // update user
     @Transactional
-    public Optional<User> updateUser(Long id, User body) { 
+    public Optional<UserDto> updateUser(Long id, User body) { 
     	if (body == null && id == null) {
     		return Optional.empty();
     	}
     	
     	return userRepository.findById(id).map(existingUser -> {
     		existingUser.setEmail(body.getEmail());
-    		
-    		return userRepository.save(existingUser);
+    		return UserMapper.toDto(userRepository.save(existingUser)); 
     	});
     	
     	

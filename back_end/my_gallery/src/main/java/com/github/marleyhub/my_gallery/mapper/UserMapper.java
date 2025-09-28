20baeb6087537cd85bed28ae5d96420ac8aa5724
cardoso <cardoso.gab@outlook.com>
2025-09-27 22:31:11 -0300

    Dto to Entity and Optitional<user> to Optional<UserDto>

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/mapper/UserMapper.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.github.marleyhub.my_gallery.mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.github.marleyhub.my_gallery.dto.UserDto;
 import com.github.marleyhub.my_gallery.entities.User;
@@ -11,14 +12,20 @@ public class UserMapper {
 		return new UserDto(user.getId(), user.getEmail(), user.getPassword());
 	}
 	
-	
 	public static List<UserDto> toDto(List<User> users){
 		if (users == null) {
 			return List.of();
 		}
-		
 		return users.stream()
 					.map(UserMapper::toDto)
 					.toList();
+	}
+	
+	public static Optional<UserDto> toDto(Optional<User> user){
+		return user.map(UserMapper::toDto);
+	}
+	
+	public static User toEntity(UserDto userDto) {
+		return new User(userDto.getId(), userDto.getEmail(), userDto.getPassword());
 	}
 }

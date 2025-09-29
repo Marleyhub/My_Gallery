0fbb0a3b03ef2324f65bd4da3df576ff4a31c66b
cardoso <cardoso.gab@outlook.com>
2025-09-29 02:22:22 -0300

    new upload service

---
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/services/S3Service.java
@@ -0,0 +1,39 @@
+package com.github.marleyhub.my_gallery.services;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+	 private final S3Client s3Client;
+	 private final String bucketName = "marley-gallery-bucket";
+	 
+	 public S3Service(S3Client s3Client) {
+		 this.s3Client = s3Client;	 
+	 }
+	 
+	 
+	 public String uploadFile(MultipartFile file) throws IOException {
+		 
+		 // media unique identifier for url safe with uuid
+		 String key = UUID.randomUUID() + "-" + file.getOriginalFilename();
+		 
+		 // param for the real put object method
+		 PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+				 .bucket(bucketName)
+				 .key(key)
+				 .contentType(file.getContentType())
+				 .build();
+		 
+		 s3Client.putObject(putObjectRequest,
+				 software.amazon.awssdk.core.sync.RequestBody.fromBytes(file.getBytes()));
+		 
+		 return "https://" + bucketName + ".s3.us-east-2.amazonaws.com/" + key;
+	 }
+}

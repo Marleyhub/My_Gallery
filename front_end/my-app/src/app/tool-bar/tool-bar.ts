baf1e1eda43b22ded4193e85ccbc890cbd41500a
cardoso <cardoso.gab@outlook.com>
2025-09-28 23:48:51 -0300

    new login to serve back end with the midia files

--- front_end/my-app/src/app/tool-bar/tool-bar.ts
+++ front_end/my-app/src/app/tool-bar/tool-bar.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,5 +12,26 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './tool-bar.scss'
 })
 export class ToolBar {
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: Event): void {
+  const input = event.target as HTMLInputElement;
+  if (!input.files?.length) {
+    return;
+  }
+  const file = input.files[0];
+  const formData = new FormData();
+  formData.append('file', file);
+
+   this.http.post<{ url: string}>('http://localhost:8080/upload', formData).subscribe({
+        next: (response) => {
+          console.log("File uploaded successfully:", response.url);
+        },
+        error: (err) => {
+          console.error('Uploaded Faild', err)
+        }
+      });
+}
+
 
 }

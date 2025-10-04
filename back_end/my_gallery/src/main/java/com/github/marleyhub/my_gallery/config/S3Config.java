fb9f853a05238e48c6dff2e22e7e6011453d1c65
cardoso <cardoso.gab@outlook.com>
2025-10-04 00:11:06 -0300

    new configuration of S3 working perfectly

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/config/S3Config.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/config/S3Config.java
@@ -3,14 +3,18 @@ package com.github.marleyhub.my_gallery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 
 @Configuration
 public class S3Config {
+	
+	private static final String DEFAULT_REGION = "us-east-2";
 	@Bean
 	S3Client s3Client() {
 		return S3Client.builder()
-					   .region(Region.US_EAST_2)
+					   .region(Region.of(DEFAULT_REGION))
+					   .credentialsProvider(DefaultCredentialsProvider.create())
 					   .build();
 	}
 }

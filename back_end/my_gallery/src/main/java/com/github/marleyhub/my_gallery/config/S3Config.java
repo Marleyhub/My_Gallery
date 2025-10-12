25c7bc5db82d54d61d80d5cda824995c8826179b
cardoso <cardoso.gab@outlook.com>
2025-10-12 16:57:23 -0300

    New config for S3Presigner

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/config/S3Config.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/config/S3Config.java
@@ -3,13 +3,16 @@ package com.github.marleyhub.my_gallery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 
+
 @Configuration
+//This tells Spring how to create S3Client and S3Presigner instances.
 public class S3Config {
-	
 	private static final String DEFAULT_REGION = "us-east-2";
+	
 	@Bean
 	S3Client s3Client() {
 		return S3Client.builder()
@@ -17,4 +20,11 @@ public class S3Config {
 					   .credentialsProvider(DefaultCredentialsProvider.create())
 					   .build();
 	}
+	
+    @Bean
+    S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.US_EAST_2)
+                .build();
+    }
 }

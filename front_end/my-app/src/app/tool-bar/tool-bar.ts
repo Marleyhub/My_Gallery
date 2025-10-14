4987c6b5c90435577e022a1ff988837477a99096
cardoso <cardoso.gab@outlook.com>
2025-10-14 18:44:40 -0300

    new logic for toolbar send a userid when uploading

--- front_end/my-app/src/app/tool-bar/tool-bar.ts
+++ front_end/my-app/src/app/tool-bar/tool-bar.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../auth/auth';
 
 @Component({
   selector: 'app-tool-bar',
@@ -12,16 +13,23 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './tool-bar.scss'
 })
 export class ToolBar {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   onFileSelected(event: Event): void {
   const input = event.target as HTMLInputElement;
   if (!input.files?.length) {
     return;
   }
+  // user id from this call
+  const user = this.authService.getUser();
+  const userId = user?.id;
+
   const file = input.files[0];
+  
+  // Form
   const formData = new FormData();
   formData.append('file', file);
+  formData.append('userId', userId);
 
    this.http.post<{ url: string}>('http://localhost:8080/uploads/upload', formData).subscribe({
         next: (response) => {

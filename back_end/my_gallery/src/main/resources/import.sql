3226c5e684a716b378da2ca1141db4369f2d8d7d
cardoso <cardoso.gab@outlook.com>
2025-10-27 15:29:03 -0300

    no more fake data db

--- back_end/my_gallery/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tb_user (email, password) VALUES ('123@gmail.com', '123456789');
-INSERT INTO tb_user (email, password) VALUES ('456@gmail.com', '123456789');
-INSERT INTO tb_user (email, password) VALUES ('789@gmail.com', '123456789');
-INSERT INTO tb_user (email, password) VALUES ('1011@gmail.com', '123456789');
-INSERT INTO tb_user (email, password) VALUES ('1213@gmail.com', '123456789');

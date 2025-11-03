13ae215cb65b3b4ef43aad053b94a4ffdcbe2609
cardoso <cardoso.gab@outlook.com>
2025-11-02 21:16:00 -0300

    adjusting s3 configurations to accept enviroment variables

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/config/S3Config.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/config/S3Config.java
@@ -1,30 +1,50 @@
 package com.github.marleyhub.my_gallery.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-
 
 @Configuration
-//This tells Spring how to create S3Client and S3Presigner instances.
+// This configuration defines how to create and inject S3Client and S3Presigner instances.
 public class S3Config {
-	private static final String DEFAULT_REGION = "us-east-2";
-	
-	@Bean
-	S3Client s3Client() {
-		return S3Client.builder()
-					   .region(Region.of(DEFAULT_REGION))
-					   .credentialsProvider(DefaultCredentialsProvider.create())
-					   .build();
-	}
-	
+
+    @Value("${AWS_ACCESS_KEY_ID}")
+    private String accessKey;
+
+    @Value("${AWS_SECRET_ACCESS_KEY}")
+    private String secretKey;
+
+    @Value("${AWS_REGION:us-east-2}")
+    private String region;
+
+    // Provides the main S3Client used for bucket and object operations.
     @Bean
-    S3Presigner s3Presigner() {
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
+                .build();
+    }
+    
+    // Provides an S3Presigner used for generating pre-signed URLs. 
+    @Bean
+    public S3Presigner s3Presigner() {
         return S3Presigner.builder()
-                .region(Region.US_EAST_2)
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
                 .build();
     }
 }

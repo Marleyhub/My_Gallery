211c80f6f6797ca6ed19bf1479165275c205bd2a
Marleyhub <cardoso.gab@outlook.com>
2025-11-22 20:24:48 -0300

    new cors config for ghpages

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/config/CorsConfig.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("https://marleyhub.github.io/My_Gallery/login")
+						.allowedOrigins("https://marleyhub.github.io/My_Gallery")
 						.allowedMethods("POST", "GET", "UPDATE", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);

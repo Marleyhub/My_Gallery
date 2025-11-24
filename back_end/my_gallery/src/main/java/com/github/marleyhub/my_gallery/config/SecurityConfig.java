2465fecc3ee0a7e89c3945916a7142910191fe74
Marleyhub <cardoso.gab@outlook.com>
2025-11-23 22:18:57 -0300

    .

--- back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/config/SecurityConfig.java
+++ back_end/my_gallery/src/main/java/com/github/marleyhub/my_gallery/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 	          .csrf(csrf -> csrf.disable())
 	          .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 	          .authorizeHttpRequests(auth -> auth
-	              .requestMatchers("/auth/**", "/public/**").permitAll()  // ONLY public endpoints
+	              .requestMatchers("/auth/**", "/public/**","/users/**").permitAll()  // ONLY public endpoints
 	              .anyRequest().authenticated()                            // everything else requires token
 	          )
 	          .userDetailsService(userDetailsService)
